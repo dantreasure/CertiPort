96f1feb699502769615dcc5b53851b434a09201e
Daniel Treasure <dbtreasure@gmail.com>
2016-01-28 20:03:18 -0700

    adds rotation animation and cursor pointer to #nav-button

--- scripts/navigation-bar.js
+++ scripts/navigation-bar.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
+	var rotated = false;
 	$("#nav-button").click(function(){
 		$('.slideout').slideToggle("slow");
+		
+		if(!rotated){
+			$(this).velocity({
+				rotateZ: "360deg"
+			},750);
+			rotated = true;
+		} else {
+			$(this).velocity({
+				rotateZ: "0deg"
+			},750);
+			rotated = false;
+		}
 	});
 });

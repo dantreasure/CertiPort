32b8a914294b437cabcbe2e5a70f24c9546d6104
Daniel Treasure <dbtreasure@gmail.com>
2016-02-02 15:49:29 -0700

    creates page heading module and reorganizes built page directories

--- scripts/local-footer.js
+++ scripts/local-footer.js
@@ -4,43 +4,43 @@ $(document).ready(function(){
 			name: "Reagan Beavers",
 			location: "Fayetteville, GA, United States<br />Student at FCHS",
 			link: "Read her story >",
-			image: "../images/students/reagan.png"
+			image: "/images/students/reagan.png"
 		},
 		{
 			name: "Ryan Miguel",
 			location: "Danbury, CT, United States<br />Product Advisor",
 			link: "Read his story >",
-			image: "../images/students/ryan.png"
+			image: "/images/students/ryan.png"
 		},
 		{
 			name: "Samantha Knupp",
 			location: "Cape Town, South Africa<br />Student at Elkanah House<br />High School",
 			link: "Read her story >",
-			image: "../images/students/samantha.png"
+			image: "/images/students/samantha.png"
 		},
 		{
 			name: "Jeffrey Crowley",
 			location: "Hoover, AL, United States<br />Student at Hoover High School",
 			link: "Read his story >",
-			image: "../images/students/jeffrey.png"
+			image: "/images/students/jeffrey.png"
 		},
 		{
 			name: "Ashley Masters",
 			location: "Kalama, WA, United States<br />Student at Kalama High School",
 			link: "Read her story >",
-			image: "../images/students/ashley.png"
+			image: "/images/students/ashley.png"
 		},
 		{
 			name: "Brandon Jacobson",
 			location: "West Jordan, United States<br />Teacher at Copper Hills<br />High School",
 			link: "Read hist story >",
-			image: "../images/students/brandon.png"
+			image: "/images/students/brandon.png"
 		},
 		{
 			name: "Chelsea",
 			location: "Piscataway, NJ, United States<br />Student at Piscataway<br />High School",
 			link: "Read her story >",
-			image: "../images/students/chelsea.png"
+			image: "/images/students/chelsea.png"
 		}
 	]
 

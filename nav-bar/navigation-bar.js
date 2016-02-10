92ffa979e8a199b92fd6274d7caa30ea1d148d44
Daniel Treasure <dbtreasure@gmail.com>
2016-02-10 11:00:39 -0700

    adjusts overlay to transition in and also adds padding to feature-pack on mos-exams page

--- nav-bar/navigation-bar.js
+++ nav-bar/navigation-bar.js
@@ -1,15 +1,7 @@
 $(document).ready(function(){
-	var globalOverlayOpen = false;
 	$(".flip-container").click(function(){
 		$(this).toggleClass("flipped")
 		$('.slideout').slideToggle(300);
-		if(!globalOverlayOpen){
-			$(".global-overlay").css({"height": "100%"});	
-			globalOverlayOpen = true;
-		} else {
-			$(".global-overlay").css({"height": "0%"});
-			globalOverlayOpen = false;
-		}
-		
+		$(".global-overlay").fadeToggle(300);	
 	});
 });

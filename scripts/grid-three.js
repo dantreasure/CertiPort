f701db591610dd36e410ad0a44f1b57debeb4fad
Daniel Treasure <dbtreasure@gmail.com>
2016-02-09 22:23:50 -0700

    updates grid three to include on hover animations

--- scripts/grid-three.js
+++ scripts/grid-three.js
@@ -1,6 +1,18 @@
 $(document).ready(function(){
 	$(".grid-three")
-		.hover(function(){
-			$(".grid-body-overlay").fadeToggle("slow", "swing");
-		})
+		.hover(
+			function(){
+				$(".grid-body-overlay").fadeToggle("slow", "swing");
+				$(".grid-body-overlay .horizontal-rule").velocity({"width": "610px"},500);
+				$(".grid-body-copy").velocity({"left":"0px"}, 500);
+				$(".grid-three .download-action").velocity({"left":"0px"}, 500);
+				$(".grid-three .visit-action").velocity({"left":"0px"}, 500);
+			},function(){
+				$(".grid-body-overlay").fadeToggle("slow", "swing");
+				$(".grid-body-overlay .horizontal-rule").velocity({"width": "0px"},200);
+				$(".grid-body-copy").velocity({"left":"650px"}, 200);
+				$(".grid-three .download-action").velocity({"left":"-250px"}, 200);
+				$(".grid-three .download-action.wide").velocity({"left":"-450px"}, 200);
+				$(".grid-three .visit-action").velocity({"left":"-400px"}, 200);
+			})
 });

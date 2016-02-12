10a26d15b7b3d5467e3318fec2661a3079e18119
Daniel Treasure <dbtreasure@gmail.com>
2016-02-11 17:52:46 -0700

    finishes key-benefits page

--- grid-three/grid-three.js
+++ grid-three/grid-three.js
@@ -7,6 +7,7 @@ $(document).ready(function(){
 				$(".grid-body-copy").velocity({"left":"0px"}, 500);
 				$(".grid-three .download-action").velocity({"left":"0px"}, 500);
 				$(".grid-three .visit-action").velocity({"left":"0px"}, 500);
+				$(".grid-three .grid-body-headline.to-white").velocity({"color": "#ffffff"},300);
 			},function(){
 				$(".grid-body-overlay").fadeToggle("slow", "swing");
 				$(".grid-body-overlay .horizontal-rule").velocity({"width": "0px"},200);
@@ -14,5 +15,6 @@ $(document).ready(function(){
 				$(".grid-three .download-action").velocity({"left":"-250px"}, 200);
 				$(".grid-three .download-action.wide").velocity({"left":"-450px"}, 200);
 				$(".grid-three .visit-action").velocity({"left":"-400px"}, 200);
+				$(".grid-three .grid-body-headline.to-white").velocity({"color": "#3b3b3c"},300);
 			})
 });

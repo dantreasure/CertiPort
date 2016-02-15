e60fe6430edca42191899bfff4346a2c468a008f
Daniel Treasure <dbtreasure@gmail.com>
2016-02-15 16:41:41 -0700

    finishes college & career success page, half-way through school success page

--- scripts/grid-three.js
+++ scripts/grid-three.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
 				$(".grid-body-overlay").fadeToggle("slow", "swing");
 				$(".grid-body-overlay .horizontal-rule").velocity({"width": "610px"},500);
 				$(".grid-body-copy").velocity({"left":"0px"}, 500);
+				$(".grid-body-list").velocity({"left":"0px"}, 500);
 				$(".grid-three .download-action").velocity({"left":"0px"}, 500);
 				$(".grid-three .visit-action").velocity({"left":"0px"}, 500);
 				$(".grid-three .grid-body-headline.to-white").velocity({"color": "#ffffff"},300);
@@ -12,6 +13,7 @@ $(document).ready(function(){
 				$(".grid-body-overlay").fadeToggle("slow", "swing");
 				$(".grid-body-overlay .horizontal-rule").velocity({"width": "0px"},200);
 				$(".grid-body-copy").velocity({"left":"650px"}, 200);
+				$(".grid-body-list").velocity({"left":"650px"}, 500);
 				$(".grid-three .download-action").velocity({"left":"-250px"}, 200);
 				$(".grid-three .download-action.wide").velocity({"left":"-450px"}, 200);
 				$(".grid-three .visit-action").velocity({"left":"-400px"}, 200);
